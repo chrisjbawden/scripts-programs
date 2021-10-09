2ea5f1909009bd84a9ecadfe4d7a3b4c9d3530b8
Chris Bawden <35166625+chrisjbawden@users.noreply.github.com>
2021-10-09 21:28:20 +1000

    Update deploy.sh

--- Linux/xmrig/deploy.sh
+++ Linux/xmrig/deploy.sh
@@ -5,9 +5,10 @@ sudo apt-get upgrade -y
 sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 if [! -d "$/miner" ]; then mkdir /miner ; fi
 cd /miner
-if [ -d "$xmrig" ]; then rm xmrig ; fi
+if [ -d "$xmrig" ]; then rm -r xmrig ; fi
 git clone https://github.com/xmrig/xmrig.git
-mkdir /xmrig/build && cd /xmrig/build
+if [ -d "$xmrig" ]; then mkdir /xmrig/build ; fi
+cd /xmrig/build
 cmake ..
 make -j$(nproc)
 wget https://github.com/chrisjbawden/scripts-programs/blob/master/Linux/xmrig/source/config.json
