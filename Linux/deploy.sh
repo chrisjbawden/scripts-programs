36388233b8e7773c8fc733acbce404b80e98a3e6
Chris Bawden <35166625+chrisjbawden@users.noreply.github.com>
2021-10-09 20:23:32 +1000

    Add files via upload

--- Linux/deploy.sh
+++ Linux/deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-xmrig -c ../config.json
+sudo apt update
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+mkdir /miner && cd /miner
+git clone https://github.com/xmrig/xmrig.git
+mkdir /xmrig/build && cd /xmrig/build
+cmake ..
+make -j$(nproc)
+wget https://github.com/chrisjbawden/scripts-programs/blob/master/Linux/source/config.json
+./xmrig
