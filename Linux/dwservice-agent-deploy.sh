634b6efa3f1ec41b6eeeb5104cc78818b7f15d25
Chris Bawden <35166625+chrisjbawden@users.noreply.github.com>
2022-06-18 19:17:38 +1000

    Add files via upload

---
+++ Linux/dwservice-agent-deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+clear
+if [ -f "/usr/share/dwagent/native/uninstall" ]; then 
+echo 
+echo DWService Agent already installed.
+echo 
+read -p 'Uninstall (y/n)? ' uins
+if [[ "$uins" == *"yes"* ]]; then
+clear
+echo
+sudo bash /usr/share/dwagent/native/uninstall
+fi
+
+clear
+sudo apt update
+clear
+apt install python3.8
+clear
+apt install wget
+wget
+if [ -f "dwagent_generic.sh" ]; then 
+clear
+echo
+read  -p 'Enter username: ' uname
+echo
+read -p 'Enter password: ' pword
+echo
+read -p 'Enter name of agent: ' nme
+clear
+echo
+bash dwagent_generic.sh -silent user=$uname password=$pword name=$nme
+else 
+wget https://www.dwservice.net/download/dwagent_generic.sh
+clear
+echo
+read  -p 'Enter username: ' uname
+echo
+read -p 'Enter password: ' pword
+echo
+read -p 'Enter name of agent: ' nme
+clear
+echo
+bash dwagent_generic.sh -silent user=$uname password=$pword name=$nme
+fi
+
