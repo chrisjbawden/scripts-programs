152913a138248c132156ac67d8619842d4b56887
Chris Bawden <35166625+chrisjbawden@users.noreply.github.com>
2023-12-31 09:23:40 +1000

    Add files via upload

---
+++ Linux/cockpit-ubuntu-manage-network-script.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt install network-manager
+sudo systemctl start NetworkManager.service
+sudo systemctl enable NetworkManager.service
+
+sudo touch /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
+
+
+#sudo sed -i '/^network:/a\  renderer: NetworkManager' /etc/netplan/00-*.yaml
+#cat /etc/netplan/00-*.yaml
+sudo rm /etc/netplan/00-*.yaml
+
+sudo service network-manager restart
